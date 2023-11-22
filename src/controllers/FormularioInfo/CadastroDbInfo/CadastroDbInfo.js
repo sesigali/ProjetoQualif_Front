454cbca4a21f8778c772b2da4ce1c4faff9a492f
Eduardo Lucas <eduardo.f@aluno.ifsp.edu.br>
2023-11-22 05:46:41 -0300

    Fim form 2

--- src/controllers/FormularioInfo/CadastroDbInfo/CadastroDbInfo.js
+++ src/controllers/FormularioInfo/CadastroDbInfo/CadastroDbInfo.js
@@ -22,78 +22,7 @@ export default function CadastroDbIndo({
   dreCompromisso,
   justRecuperacaoCompromisso,
 }) {
-  const [formData] = useState({
-    certidao: {
-      columns: ['planoRecuperacao', 'naturezaCertidao', 'certidaoFalencia', 'idEmpresa'],
-      data: {
-        planoRecuperacao: docRecuperacaoCertidao,
-        naturezaCertidao: certidaoNaturezaCertidao,
-        certidaoFalencia: anexoCertidao,
-        idEmpresa,
-      },
-    },
-    balanco: {
-      columns: ['conformidadeLei', 'balanco', 'idEmpresa'],
-      data: {
-        conformidadeLei: balancoConfLeiBalanco,
-        balanco: anexoBalanco,
-        idEmpresa,
-      },
-    },
-    indice: {
-      columns: [
-        'ativoCirculante',
-        'ativoReaLongoPrazo',
-        'ativoTotal',
-        'passivoCirculante',
-        'passivoNaoCirculante',
-        'patrimonioLiquido',
-        'idEmpresa',
-      ],
-      data: {
-        ativoCirculante: ativoCirculanteIndice,
-        ativoReaLongoPrazo: ativoReaLongoPrazoIndice,
-        ativoTotal: ativoTotalIndice,
-        passivoCirculante: passivoCirculanteIndice,
-        passivoNaoCirculante: passivoNaoCirculanteIndice,
-        patrimonioLiquido: patrimonioLiquidoIndice,
-        idEmpresa: idEmpresa,
-      },
-    },
-    complementacao: {
-      columns: ['comprAssumidos', 'idEmpresa'],
-      data: {
-        comprAssumidos: compromissosAssumidos,
-        idEmpresa,
-      },
-    },
-    compromisso: {
-      columns: [
-        'receitaBruta',
-        'declaracaoCompr',
-        'dre',
-        'justificativa',
-        'idEmpresa',
-      ],
-      data: {
-        receitaBruta: receitaBrutaCompromisso,
-        declaracaoCompr: declaracaoCompromisso,
-        dre: dreCompromisso,
-        justificativa: justRecuperacaoCompromisso,
-        idEmpresa,
-      },
-    },
-    //idEmpresa,
-  });
-  console.log('Estado Atual do Formulário1:', formData);
-  console.log('Estado Atual do idEmpresa:', formData.idEmpresa);
-  console.log('Estado Atual do balanco:', formData.balanco);
-  console.log('Estado Atual do certidao:', formData.certidao);
-  console.log('Estado Atual do indice:', formData.indice);
-  console.log('Estado Atual do complementacao:', formData.complementacao);
-  console.log('Estado Atual do balanco.conformidadeLei:', formData.balanco.conformidadeLei);
 
-//--------------------------------------------------------------------------------------------
 const [mensagem, setMensagem] = useState(null);
 
 const enviarTodasAsInformacoes = async () => {
@@ -107,7 +36,6 @@ const enviarTodasAsInformacoes = async () => {
 
 //Certidao
 const handleCadastroCertidao = async () => {
-  
 
   const certidaos = {
     planoRecuperacao: docRecuperacaoCertidao,
@@ -128,7 +56,6 @@ const handleCadastroCertidao = async () => {
 
 //Balanço
 const handleCadastroBalanco = async (event) => {
-  
 
   const balancos = {
     conformidadeLei: balancoConfLeiBalanco,
@@ -148,7 +75,6 @@ const handleCadastroBalanco = async (event) => {
 
 //Indices
 const handleCadastroIndice = async (event) => {
-  
 
   const indices = {
     ativoCirculante: ativoCirculanteIndice,
@@ -173,7 +99,6 @@ const handleCadastroIndice = async (event) => {
 //Complementação
 const handleCadastroComplementacao = async (event) => {
   
-
   const complementacaos = {
     comprAssumidos: compromissosAssumidos,
     idEmpresa: idEmpresa,
@@ -192,7 +117,6 @@ const handleCadastroComplementacao = async (event) => {
 //Compromissos
 const handleCadastroCompromissos = async (event) => {
   
-
   const compromissos = {
     receitaBruta: receitaBrutaCompromisso,
     declaracaoCompr: declaracaoCompromisso,
@@ -217,68 +141,8 @@ const handleSubmit1 = async (event) => {
   await enviarTodasAsInformacoes();
 
   // Adicione lógica adicional após o envio de todas as informações, se necessário.
-  setMensagem('Todas as informações foram enviadas com sucesso!');
+  //setMensagem('Todas as informações foram enviadas com sucesso!');
 };
-
-
-
-//---------------------------------------------------------------------------------------------
-
-
-
-  
-/*   const handleFileChange = (e, section, name) => {
-    const file = e.target.files[0];
-    formData[section].data[name] = file;
-    console.log(`Arquivo ${name} adicionado à seção ${section}:`, file);
-  }; */
-
-  const handleSubmit = async () => {
-    try {
-      console.log('Estado Atual do Formulário2:', formData);
-      for (const key in formData) {
-        const section = formData[key];
-        const columns = section.columns;
-        const data = section.data;
-
-        const requestData = {};
-        columns.forEach((column) => {
-/*           if (data[column]) {
-            requestData[column] = data[column];
-          } else {
-            console.error(`Valor vazio para ${column}`);
-          } */
-          
-          requestData[column] = data[column];
-        });
-        console.log(`Dados que serão enviados para ${key}:`, requestData);
-
-
-        //if (Object.values(requestData).every(value => value !== undefined && value !== '')) {
-          const response = await axios.post(`http://localhost:8888/${key}/adicionar`, {
-            ...requestData,
-            //idEmpresa: formData.idEmpresa,
-          });
-
-          console.log(`Dados de ${key} enviados com sucesso!`, response);
-        /* } else {
-          console.error(`Erro ao enviar dados para ${key}: Parâmetros faltando ou vazios.`);
-        } */
-      /*
-        const response = await axios.post(`http://localhost:8888/${key}/adicionar`, {
-          ...requestData,
-          idEmpresa: formData.idEmpresa,
-        });
-
-        console.log(`Dados de ${key} enviados com sucesso!`, response.data); */
-
-
-
-      }
-    } catch (error) {
-      console.error('Erro ao enviar dados:', error);
-    }
-  };
 
   return (
     <div>
@@ -302,14 +166,9 @@ const handleSubmit1 = async (event) => {
       <br />
       <br />
       <div className="container-button">
-        <button className="save btn-empresaInfo" type="button" onClick={handleSubmit}>
-          Salvar
-        </button>
-
         <Link onSubmit={handleSubmit1} to={`/relatorioInfo/${idEmpresa}`}>
-          <button className="save btn-empresaInfo" type="button">Enviar Todas as Informações</button>
+          <button className="save btn-empresaInfo" type="button">Salvar</button>
         </Link>
-        <div>{mensagem && <p>{mensagem}</p>}</div>
 
         <Link to="/">
           <button className="cancel btn-empresaInfo">Cancelar</button>
